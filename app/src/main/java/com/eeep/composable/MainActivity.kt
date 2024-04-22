5bc22f08f508999f10807e655724bf2816e955e7
Israel <yssrael@gmail.com>
2024-04-22 14:56:20 -0300

    first commit

---
+++ app/src/main/java/com/eeep/composable/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.eeep.composable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eeep.composable.ui.theme.ComposableTheme
+import com.eeep.ocorrencia1.views.ButtonM3
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposableTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ButtonM3()
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposableTheme {
+        ButtonM3()
+    }
+}
---
+++ app/src/main/java/com/eeep/composable/views/ButtonM3.kt
@@ -0,0 +1,29 @@
+package com.eeep.ocorrencia1.views
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ButtonM3(){
+    Column {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ){
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                }){
+                Text(text = "ADicionar")
+
+            }
+        }
+
+    }
+}
